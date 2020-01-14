1326ca3fc3125629c6265463853f5e14d8568098
Ciaran <ccoady@tcd.ie>
2020-01-14 17:05:47 +0000

    fixed typo in description

--- my-portfolio-app/src/HomePage.js
+++ my-portfolio-app/src/HomePage.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import ProfilePicture from './ProfilePicture.jpg';
-import github from './github.png';
-import linkedin from './linkedin.png';
-import './HomePage.css';
+import React from "react";
+import ProfilePicture from "./ProfilePicture.jpg";
+import github from "./github.png";
+import linkedin from "./linkedin.png";
+import "./HomePage.css";
 
 function HomePage() {
   return (
     <div className="HomePage">
       <header className="HomePage-header">
-        <img src={ProfilePicture} className="Profile-Picture" alt="ProfilePicture" />
+        <img
+          src={ProfilePicture}
+          className="Profile-Picture"
+          alt="ProfilePicture"
+        />
         <p className="Bio-Text">
-          Hi I'm Ciarán, A Computer Science student with a passion tech, challenges and adventure.
+          Hi I'm Ciarán, A Computer Science student with a passion for tech,
+          challenges and adventure.
         </p>
-        <p className="Bio-Text">
-          Checkout my github to see some of my work.
-        </p>
-        <p/>
+        <p className="Bio-Text">Checkout my github to see some of my work.</p>
+        <p />
         <div className="IconContainer">
-        <a href="http://www.github.com/ciarancoady98">
-          <img src={github} className="github" alt="github"/>
-        </a>
-        <a href="https://www.linkedin.com/in/ciarancoady/">
-          <img src={linkedin} className="linkedin" alt="linkedin"/>
-        </a>
+          <a href="http://www.github.com/ciarancoady98">
+            <img src={github} className="github" alt="github" />
+          </a>
+          <a href="https://www.linkedin.com/in/ciarancoady/">
+            <img src={linkedin} className="linkedin" alt="linkedin" />
+          </a>
         </div>
       </header>
     </div>
