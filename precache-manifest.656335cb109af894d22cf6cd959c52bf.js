0ed1385a20e3b655d98c7135248b8e8aa3a50217
Ciaran <ccoady@tcd.ie>
2020-01-14 17:39:28 +0000

    fixed typo in live website

---
+++ precache-manifest.656335cb109af894d22cf6cd959c52bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8a469f30369afe71c310fb8f779580",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07f75fd6b5c17219032c",
+    "url": "/static/css/main.04202bc0.chunk.css"
+  },
+  {
+    "revision": "6577345ab4ba516ce501",
+    "url": "/static/js/2.342a7cb2.chunk.js"
+  },
+  {
+    "revision": "07f75fd6b5c17219032c",
+    "url": "/static/js/main.bf0dd6c8.chunk.js"
+  },
+  {
+    "revision": "34f09b7dba2b885704b6",
+    "url": "/static/js/runtime-main.84d2b9ea.js"
+  },
+  {
+    "revision": "909cb3a0bcb6383ad0642daebf9d2298",
+    "url": "/static/media/ProfilePicture.909cb3a0.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.782c156ec24b4e86bd50ab997fa8a8b4.js"
+  "/precache-manifest.656335cb109af894d22cf6cd959c52bf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
