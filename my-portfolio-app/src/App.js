ca2277383556749b3c0e37e50d90cd0126da4160
ciaran <ccoady@tcd.ie>
2020-05-16 17:43:35 +0100

    creating a clean react project using the latest template and yarn

--- my-portfolio-app/src/App.js
+++ my-portfolio-app/src/App.js
@@ -1,53 +1,26 @@
-import React, { Component } from "react";
-import ReactGA from "react-ga";
-import $ from "jquery";
-import "./App.css";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import About from "./Components/About";
-import Work from "./Components/Work";
-import Contact from "./Components/Contact";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pageData: {}
-    };
-    ReactGA.initialize("UA-110570651-1");
-    ReactGA.pageview(window.location.pathname);
-  }
-
-  getResumeData() {
-    $.ajax({
-      url: "/data.json",
-      dataType: "json",
-      cache: false,
-      success: function(data) {
-        this.setState({ pageData: data });
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.log(err);
-        alert(err);
-      }
-    });
-  }
-
-  componentDidMount() {
-    this.getResumeData();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <Header data={this.state.pageData.main} />
-        {/* <About data={this.state.pageData.main} /> */}
-        <Work data={this.state.pageData.resume} />
-        {/* <Contact data={this.state.pageData.main} /> */}
-        <Footer data={this.state.pageData.main} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
